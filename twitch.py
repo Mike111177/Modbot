fb43c0356ec2bdbc9b8a60ca8252c13d444608d0
Mike111177 <captain@mikeddavidson.com>
2016-11-11 15:24:51 -0500

    Deprecated old twitch api.

--- twitch.py
+++ twitch.py
@@ -3,13 +3,13 @@ import json
 
 cid = None
 
-def getChatters(name, cid=cid):
+def getChatters(name, cid=cid): #Deprecated
 	return json.loads((request.urlopen("http://tmi.twitch.tv/group/user/%s/chatters"%name).read().decode("utf-8")))["chatters"]["viewers"]
 
-def getUserDisplayName(user, cid=cid):
+def getUserDisplayName(user, cid=cid):#Deprecated
 	return getUserData(user, cid)['display_name']
 
-def getUserData(name, cid=cid):
+def getUserData(name, cid=cid):#Deprecated
 	try:
 		req = request.Request("https://api.twitch.tv/kraken/users/%s"%name,  headers={'Client-ID': cid}) #LOOK AT ME IM TWITCH! I REQUIRE A FUCKING CLIENT ID NOW TO USE MY API
 		return json.loads(request.urlopen(req).read().decode())
