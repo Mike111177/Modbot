78ac8d0b5b33580ffe1dbaf2fe535dea129180da
Mike111177 <captain@mikeddavidson.com>
2016-12-02 19:52:12 -0500

    Added ability to artificially revoke subscribers privilege to post links provided by nightbot.

--- plugins/heuristicmod.py
+++ plugins/heuristicmod.py
@@ -1,4 +1,4 @@
-import traceback, asyncio, re, argparse
+import traceback, asyncio, re, argparse, pickle
 from components import abstracts, pluginmanager, config
 from math import floor
 from datetime import timedelta, datetime
@@ -19,15 +19,34 @@ reglink = re.compile(rlinkstring, re.IGNORECASE)
 
 bncntparse = argparse.ArgumentParser(description='Count automated bot bans.', add_help=False, prog='?bancount')
 bncntparse.add_argument('time', type=float, default=float(1), help='Time back to count. (default is 1)', nargs='?')
-group = bncntparse.add_mutually_exclusive_group()
-group.add_argument('-s', action='store_true', help='Set time multiplier to seconds.')
-group.add_argument('-m', action='store_true', help='Set time multiplier to minutes.')
-group.add_argument('-h', action='store_true', help='Set time multiplier to hours (default).')
-group.add_argument('-d', action='store_true', help='Set time multiplier to days.')
-group.add_argument('-w', action='store_true', help='Set time multiplier to weeks.')
-
+tgroup = bncntparse.add_mutually_exclusive_group()
+tgroup.add_argument('-s', action='store_true', help='Set time multiplier to seconds.')
+tgroup.add_argument('-m', action='store_true', help='Set time multiplier to minutes.')
+tgroup.add_argument('-h', action='store_true', help='Set time multiplier to hours (default).')
+tgroup.add_argument('-d', action='store_true', help='Set time multiplier to days.')
+tgroup.add_argument('-w', action='store_true', help='Set time multiplier to weeks.')
 
 permitcache = {}
+
+revokeparse = argparse.ArgumentParser(description='Manage users revoked from posting links.', add_help=False, prog='?revoke')
+subparsers = revokeparse.add_subparsers(dest='cmd')
+addparse = subparsers.add_parser('add', help='Add a user to revoke list.')
+addparse.add_argument('user', type=str, help='The user.')
+remparse = subparsers.add_parser('remove', help='Remove a user to revoke list.')
+remparse.add_argument('user', type=str, help='The user.')
+subparsers.add_parser('list', help='List the users on the revoke list.')
+
+def saveRevokeList():
+    with open(revokelistname, 'wb') as output:
+        pickle.dump(revokelist, output)
+revokelistname="data/revokelist"
+try:
+    with open(revokelistname, 'rb') as file:
+        revokelist = pickle.load(file)
+except:
+    revokelist = set()
+    saveRevokeList()
+
 
 def containsFailedLink(message):
     if regflink.search(message):
@@ -59,19 +78,23 @@ def containsSpecial(message):
         return True
     else:
         return False
-
-def isSpamBot(name, message, cid):
+    
+def nightCheck(name, message):
     if name.lower()=='nightbot':
         me = regperm.match(message)
         if me:
             permitcache[me.group('Name').lower()] = clock()
-        return False, None
-    
-    if name.lower() in permitcache and clock()-permitcache[name.lower()]<300:
+        return True
+    return False
+
+def permitCheck(name):
+    return name.lower() in permitcache and clock()-permitcache[name.lower()]<70
+
+def isSpamBot(name, message, cid):
+    if permitCheck(name):
         links = False
     else:
         links = containsLink(message) or containsFailedLink(message)
-        
         
     addr = containsSpecial(message)
     if links or addr:
@@ -90,6 +113,7 @@ class Plugin(abstracts.Plugin):
     def handlers(self):
         return [abstracts.Handler('TWITCH:MSG', self, self.ircmsg),
                 abstracts.Handler('DSC:COMMAND:?BANCOUNT', self, self.bancnt, parse=bncntparse),
+                abstracts.Handler('DSC:COMMAND:?REVOKE', self, self.revoke, parse=revokeparse),
                 abstracts.Handler('TWITCH:MOD:TIMEOUT', self, self.timeout)]
     
     def timeout(self, created_by=None, args=None, **kw):
@@ -108,14 +132,18 @@ class Plugin(abstracts.Plugin):
     def ircmsg(self, nick=None, target=None, data=None, **kw):
         cid = pluginmanager.resources["TWITCH"]["CLI-ID"]
         try:
-            t = clock()
-            spam, age = isSpamBot(nick, data, cid)
-            if spam:
-                bot = pluginmanager.resources["TWITCH"]["BOT"]
-                disbot = pluginmanager.resources["DSC"]["BOT"]
-                loop = pluginmanager.resources["DSC"]["LOOP"]
-                bot.privmsg(target, '.ban %s'%nick)
-                asyncio.run_coroutine_threadsafe(disbot.send_message(disbot.get_channel(str(cfg['Reporting']['AutoChannel'])),'%s: "%s" (Age: %s) [%fs]'%(nick,data,str(timedelta(seconds=floor(age))),clock()-t)), loop)
+            if not nightCheck(nick, data) and not permitCheck(nick):
+                t = clock()
+                spam, age = isSpamBot(nick, data, cid)
+                if spam:
+                    bot = pluginmanager.resources["TWITCH"]["BOT"]
+                    disbot = pluginmanager.resources["DSC"]["BOT"]
+                    loop = pluginmanager.resources["DSC"]["LOOP"]
+                    bot.privmsg(target, '.ban %s'%nick)
+                    asyncio.run_coroutine_threadsafe(disbot.send_message(disbot.get_channel(str(cfg['Reporting']['AutoChannel'])),'%s: "%s" (Age: %s) [%fs]'%(nick,data,str(timedelta(seconds=floor(age))),clock()-t)), loop)
+                elif nick.lower() in revokelist and containsLink(data):
+                    bot = pluginmanager.resources["TWITCH"]["BOT"]
+                    bot.privmsg(target, '.timeout %s 60'%nick)                
         except:
             print('Error in spambot filter.\nUser: %s\nMessage: "%s"'%(nick,data))
             print(traceback.format_exc())
@@ -123,7 +151,6 @@ class Plugin(abstracts.Plugin):
             if clock()-self.refreshtime>43200:
                 self.msglog = {}
             self.msglog[nick] = data
-        
         
     def bancnt(self, message=None, args=None, pargs=None, **kw):
         disbot = pluginmanager.resources["DSC"]["BOT"]
@@ -152,3 +179,20 @@ class Plugin(abstracts.Plugin):
             if m.author == disbot.user:
                 counter += 1
         await disbot.send_message(channel, 'There have been %d automated bans in the past %s %s(s).'%(counter, pargs['time'], mod))
+        
+    def revoke(self, message=None, pargs=None,**kw):
+        msg = None
+        if pargs['cmd']=='list':
+            msg = '```'+str(list(revokelist))+'```'
+        elif pargs['cmd']=='add':
+            revokelist.add(pargs['user'].lower())
+            saveRevokeList()
+            msg = '%s is now on the revoke list.'%pargs['user']
+        elif pargs['cmd']=='remove':
+            revokelist.remove(pargs['user'].lower())
+            saveRevokeList()
+            msg = '%s is now not on the revoke list.'%pargs['user']
+        if msg:
+            bot = pluginmanager.resources["DSC"]["BOT"]
+            loop = pluginmanager.resources["DSC"]["LOOP"]
+            asyncio.run_coroutine_threadsafe(bot.send_message(message.channel, msg), loop)
